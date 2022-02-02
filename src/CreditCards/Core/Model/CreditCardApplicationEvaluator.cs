1630a5aabf6306fb44da69bc14031c681f16cd7f
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-02-02 16:43:46 +0100

    Add CreditCardApplicationDecision model

---
+++ src/CreditCards/Core/Model/CreditCardApplicationEvaluator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCards.Core.Model
+{
+    public class CreditCardApplicationEvaluator
+    {
+        private const int HighIncomeThreshhold = 100_000;
+        private const int AutoReferralMaxAge = 20;
+        private const int LowIncomeThreshhold = 20_000;
+
+        public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
+        {
+            if (application.GrossAnnualIncome >= HighIncomeThreshhold)
+            {
+                return CreditCardApplicationDecision.AutoAccepted;
+            }
+
+            if (application.GrossAnnualIncome <= AutoReferralMaxAge)
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
+            }
+
+            if (application.GrossAnnualIncome < LowIncomeThreshhold)
+            {
+                return CreditCardApplicationDecision.AutoDeclined;
+            }
+
+            return CreditCardApplicationDecision.ReferredToHuman;
+        }
+    }
+}
