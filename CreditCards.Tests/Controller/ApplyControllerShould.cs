1b54fe53d580487be598e4962fdc1907fdd12bee
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-02-03 16:07:37 +0100

    First Controller Test

---
+++ CreditCards.Tests/Controller/ApplyControllerShould.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Moq;
+using System;
+using CreditCards.Controllers;
+using CreditCards.Core.Model;
+using CreditCards.Core.Interfaces;
+using CreditCards.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditCards.Tests.Controller
+{
+    public class ApplyControllerShould
+    {
+        private readonly Mock<ICreditCardApplicationRepository> _mockRepository;
+        private readonly ApplyController _sut;
+        public ApplyControllerShould()
+        {
+            _mockRepository = new Mock<ICreditCardApplicationRepository>();
+            _sut = new ApplyController(_mockRepository.Object);
+        }
+
+        [Fact]
+        public void ReturnViewForIndex()
+        {
+            IActionResult result = _sut.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
