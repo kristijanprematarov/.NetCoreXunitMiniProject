df3208031b8416331ccfe286f6f378ee4708e032
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-02-03 17:02:38 +0100

    Testing API Value Controllers

---
+++ CreditCards.Tests/Controller/ValuesControllerShould.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using Moq;
+using CreditCards.Controllers;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditCards.Tests.Controller
+{
+    public class ValuesControllerShould
+    {
+        private readonly ValuesController _sut;
+        public ValuesControllerShould()
+        {
+            _sut = new ValuesController();
+        }
+
+        [Fact]
+        public void ReturnValues()
+        {
+            string[] result = _sut.Get().ToArray();
+
+            Assert.Equal("value1", result[0]);
+            Assert.Equal("value2", result[1]);
+        }
+
+        [Fact]
+        public void ReturnBadRequest()
+        {
+            IActionResult result = _sut.Get(0);
+
+            var badRequest = Assert.IsType<BadRequestObjectResult>(result);
+
+            Assert.Equal("Invalid request for id: 0", badRequest.Value);
+        }
+
+        [Fact]
+        public void StartJobOk()
+        {
+            IActionResult result = _sut.StartJob();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            Assert.Equal("Batch Job Started", okResult.Value);
+        }
+
+       
+    }
+}
