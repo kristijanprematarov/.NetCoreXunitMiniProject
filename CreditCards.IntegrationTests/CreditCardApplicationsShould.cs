221ef5b081911b8806b94947766e16aa9918ad2b
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-02-05 23:26:07 +0100

    Testing Post Operations Remember THIS, with errors for antiforgery token, fixed in next commit

--- CreditCards.IntegrationTests/CreditCardApplicationsShould.cs
+++ CreditCards.IntegrationTests/CreditCardApplicationsShould.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
 using Xunit;
+using System.Collections.Generic;
 
 namespace CreditCards.IntegrationTests
 {
@@ -34,6 +35,42 @@ namespace CreditCards.IntegrationTests
             var responseString = await response.Content.ReadAsStringAsync();
 
             Assert.Contains("New Credit Card Application", responseString);
+        }
+
+        [Fact]
+        public async Task NotAcceptPostedApplicationDetailsWithMissingFrequentFlyerNumber()
+        {
+            var builder = new WebHostBuilder()
+                .UseContentRoot(@"C:\Users\Kristijan\source\repos\.NET Core MVC Testing Fundamentals\src\CreditCards")
+                .UseEnvironment("DEVELOPMENT")
+                .UseStartup<CreditCards.Startup>()
+                .UseApplicationInsights();
+
+            var server = new TestServer(builder);
+
+            var client = server.CreateClient();
+
+            HttpRequestMessage postRequest =
+                new HttpRequestMessage(HttpMethod.Post, "/Apply");
+
+            var formData = new Dictionary<string, string>()
+            {
+                {"FirstName","Sarah" },
+                {"LastName","Smith" },
+                {"Age","18" },
+                {"GrossAnnualIncome","100000" },
+                //FREQUENT FLYER NUMBER NOT ADDED a.k.a. MISSING
+            };
+
+            postRequest.Content = new FormUrlEncodedContent(formData);
+
+            HttpResponseMessage responseFromPost = await client.SendAsync(postRequest);
+
+            responseFromPost.EnsureSuccessStatusCode();
+
+            var responseString = await responseFromPost.Content.ReadAsStringAsync();
+
+            Assert.Contains("Please provide a frequent flyer number", responseString);
         }
     }
 }
