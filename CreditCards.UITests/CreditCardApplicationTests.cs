117e8540897b3c123e6fb83caed98371c2f66079
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-02-06 20:23:01 +0100

    Remove Selenium if i need it i will learn it

--- CreditCards.UITests/CreditCardApplicationTests.cs
+++ CreditCards.UITests/CreditCardApplicationTests.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using Xunit;
 
 namespace CreditCards.UITests
 {
-    public class CreditCardApplicationTests
+    public class CreditCardApplicationTests : IDisposable
     {
+        private readonly IWebDriver _driver;
 
+        public CreditCardApplicationTests()
+        {
+            _driver = new EdgeDriver();
+        }
+
+        [Fact]
+        public void ShouldLoadApplicationPage_SmokeTest()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:44108/Apply");
+
+            Assert.Equal("Credit Card Application - CreditCards", _driver.Title);
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
     }
 }
