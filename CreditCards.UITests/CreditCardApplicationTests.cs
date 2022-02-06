567929d35afb345bf63f85dfac9a5f40649a4a23
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2022-02-06 20:03:30 +0100

    Selenium Getting started

---
+++ CreditCards.UITests/CreditCardApplicationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardApplicationTests
+    {
+
+    }
+}
